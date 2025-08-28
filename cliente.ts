7aa27a98907a6c0091e91431764644336d730b67
rdg <henrique_rodeghiero@hotmail.com>
2025-08-28 09:32:35 -0300

    primeiro commit

--- cliente.ts
+++ cliente.ts
@@ -1,4 +1,18 @@
 export class Cliente {
     nome: string = "";
     telefone: string = "";
-} 
+
+    constructor(){
+        this.nome = "";
+        this.telefone = "";
+    }
+
+    criarCliente(nome: string, telefone: string): Cliente {
+        if (nome.length < 3){
+            throw new Error("Nome inválido");
+        }
+        this.nome = nome;
+        this.telefone = telefone;
+        return this;
+    }
+}
