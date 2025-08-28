93051624b8f19512c61610eed07dccbc0660b56f
rdg <henrique_rodeghiero@hotmail.com>
2025-08-28 09:40:28 -0300

    third commit

--- Util.ts
+++ Util.ts
@@ -2,5 +2,11 @@ import { faker } from '@faker-js/faker';
 export class Util {
     gerarNome(): string {
         const randomName = faker.person.firstName();
+        return randomName;
     }
 }
+
+const u: Util = nem Util();
+const nomeGerado = u.gerarNome();
+
+console.log
